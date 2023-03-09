bc86975e09a38bc300c76341218f716284df481e
Sakkarin1202 <xfake12340@hotmail.com>
2023-03-09 10:31:15 +0700

    update

--- index.js
+++ index.js
@@ -62,3 +62,12 @@ var swiper = new Swiper(".review-slider", {
         },
     }
 });
+
+/*Loader Section*/
+function loader(){
+    document.querySelector(".loader-container").classList.add("fade-out")
+}
+function fadeOut(){
+    setInterval(loader, 3000)
+}
+window.onload = fadeOut;
